5befb2d99de594591b2c2d9edcbd42863158b0b2
Igor Paprzycki <igypap@gmail.com>
2017-02-19 23:23:35 +0100

    Initial commit

---
+++ app/src/main/java/com/igypap/youtubevideos/activity/YoutubeActivity.java
@@ -0,0 +1,123 @@
+package com.igypap.youtubevideos.activity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+import com.igypap.youtubevideos.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class YoutubeActivity extends YouTubeBaseActivity
+        implements YouTubePlayer.OnInitializedListener {
+
+    @BindView(R.id.youtube_player)
+    YouTubePlayerView youTubePlayerView;
+
+    private String GOOGLE_API_KEY = "TBA";
+    private String YOUTUBE_VIDEO_ID = "TBA";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_youtube);
+        ButterKnife.bind(this);
+
+        youTubePlayerView.initialize(GOOGLE_API_KEY, this);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                        YouTubePlayer youTubePlayer,
+                                        boolean wasRestored) {
+        Toast.makeText(this, "Youtube player initialized successfully", Toast.LENGTH_SHORT).show();
+        youTubePlayer.setPlayerStateChangeListener(playerStateChangedListener);
+        youTubePlayer.setPlaybackEventListener(playbackEventListener);
+
+        if (!wasRestored){
+            youTubePlayer.cueVideo(YOUTUBE_VIDEO_ID);
+        }
+    }
+
+    private YouTubePlayer.PlaybackEventListener playbackEventListener =
+            new YouTubePlayer.PlaybackEventListener() {
+        @Override
+        public void onPlaying() {
+
+        }
+
+        @Override
+        public void onPaused() {
+
+        }
+
+        @Override
+        public void onStopped() {
+
+        }
+
+        @Override
+        public void onBuffering(boolean b) {
+
+        }
+
+        @Override
+        public void onSeekTo(int i) {
+
+        }
+    };
+
+    private YouTubePlayer.PlayerStateChangeListener playerStateChangedListener =
+            new YouTubePlayer.PlayerStateChangeListener() {
+                @Override
+                public void onLoading() {
+
+                }
+
+                @Override
+                public void onLoaded(String s) {
+
+                }
+
+                @Override
+                public void onAdStarted() {
+
+                }
+
+                @Override
+                public void onVideoStarted() {
+
+                }
+
+                @Override
+                public void onVideoEnded() {
+
+                }
+
+                @Override
+                public void onError(YouTubePlayer.ErrorReason errorReason) {
+
+                }
+            };
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                        YouTubeInitializationResult youTubeInitializationResult) {
+        Toast.makeText(this, "Failure initializing youtube player", Toast.LENGTH_SHORT).show();
+    }
+}
