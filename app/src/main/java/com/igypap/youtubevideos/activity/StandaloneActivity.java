eff60a77f46f6a999a1d8516902649d7cef7010b
Igor Paprzycki <igypap@gmail.com>
2017-02-20 23:17:15 +0100

    Added Butterknife binding to Standalone Activity

--- app/src/main/java/com/igypap/youtubevideos/activity/StandaloneActivity.java
+++ app/src/main/java/com/igypap/youtubevideos/activity/StandaloneActivity.java
@@ -10,31 +10,29 @@ import android.widget.Button;
 import com.google.android.youtube.player.YouTubeStandalonePlayer;
 import com.igypap.youtubevideos.R;
 
-public class StandaloneActivity extends AppCompatActivity
-        implements View.OnClickListener {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class StandaloneActivity extends AppCompatActivity {
 
     private String GOOGLE_API_KEY = "AIzaSyBiAP0jLSDvJiSJp03qXlKWgXJWVOiNLyY";
     private String YOUTUBE_VIDEO_ID = "rnWAPG6wkco";
     private String YOTUBE_PLAYLIST = "PL66120AF13865BBF9";
 
-    //    @BindView(R.id.btn_play_video)
+    @BindView(R.id.btn_play_video)
     Button mBtnPlayVideo;
-    //    @BindView(R.id.btn_playlist)
+    @BindView(R.id.btn_playlist)
     Button mBtnPlayPlaylist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_standalone);
-//        ButterKnife.bind(this);
+        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        mBtnPlayVideo = (Button) findViewById(R.id.btn_play_video);
-        mBtnPlayPlaylist = (Button) findViewById(R.id.btn_playlist);
-
-        mBtnPlayVideo.setOnClickListener(this);
-        mBtnPlayPlaylist.setOnClickListener(this);
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -47,8 +45,7 @@ public class StandaloneActivity extends AppCompatActivity
     }
 
 
-    //    @OnClick({R.id.btn_play_video, R.id.btn_playlist})
-    @Override
+    @OnClick({R.id.btn_play_video, R.id.btn_playlist})
     public void onClick(View v) {
         Intent intent = null;
         switch (v.getId()) {
