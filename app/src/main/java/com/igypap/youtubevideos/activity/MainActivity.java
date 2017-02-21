b0cc69c0d7f7d4f17ec15870eb38b3d7d9679193
Igor Paprzycki <igypap@gmail.com>
2017-02-21 23:09:16 +0100

    Main activity with options: 1. YouTube activity single video. 2. Standalone activity with single video and playlist playing

--- app/src/main/java/com/igypap/youtubevideos/activity/MainActivity.java
+++ app/src/main/java/com/igypap/youtubevideos/activity/MainActivity.java
@@ -1,20 +1,34 @@
 package com.igypap.youtubevideos.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.igypap.youtubevideos.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_single_video)
+    Button mBtnPlaySingle;
+    @BindView(R.id.btn_standalone)
+    Button mBtnStandaloneMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -29,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -46,5 +60,20 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick({R.id.btn_single_video, R.id.btn_standalone})
+    public void onClick(View v){
+        Intent intent = null;
+        switch(v.getId()){
+            case R.id.btn_single_video:
+                intent = new Intent(this, YoutubeActivity.class);
+                break;
+            case R.id.btn_standalone:
+                intent = new Intent(this, StandaloneActivity.class);
+                break;
+            default:
+        }
+        startActivity(intent);
     }
 }
